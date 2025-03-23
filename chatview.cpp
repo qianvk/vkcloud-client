93e3774afc202ab8442ce2ed1f1837bc3dd2f2fb
qianvk <qianvkk@gmail.com>
2025-03-23 17:17:47 +0800

    complete chat_view

--- chatview.cpp
+++ chatview.cpp
@@ -1,5 +1,17 @@
 #include "chatview.h"
 
+#include <QVBoxLayout>
+#include <QScrollArea>
+
 ChatView::ChatView(QWidget *parent)
     : QWidget{parent}
-{}
+    , is_loading_bottom_(false)
+    , p_chat_area_(new QScrollArea())
+{
+    QVBoxLayout *p_main_layout = new QVBoxLayout();
+    this->setLayout(p_main_layout);
+    p_main_layout->setContentsMargins(0, 0, 0, 0);
+
+    p_chat_area_->setObjectName("chat_area");
+    p_main_layout->addWidget(p_chat_area_);
+}
--- chatview.h
+++ chatview.h
@@ -1,11 +1,19 @@
 #ifndef CHATVIEW_H
 #define CHATVIEW_H
 
+#include <QWidget>
+
+class QScrollArea;
+
 class ChatView : public QWidget
 {
     Q_OBJECT
 public:
     explicit ChatView(QWidget *parent = nullptr);
+
+private:
+    bool is_loading_bottom_;
+    QScrollArea *p_chat_area_;
 
 signals:
 };
